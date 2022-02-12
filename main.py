38ce469f2bc60abe8c066bc5d19fb70b4ebdb1b1
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-02-11 17:24:02 -0800

    added response for advanced stats

--- main.py
+++ main.py
@@ -14,8 +14,81 @@ BASE = "http://127.0.0.1:5000/"
 SEASON = 2021
 NBA_API = f"http://data.nba.net/data/10s/prod/v1/{SEASON}/players.json"
 
+# parse !gopher-basic command and creates embed
 
-@client.event
+
+def parse_command_basic(message):
+    message = message[14:]
+    message_split = message.split(' ')
+    stat_time = message_split[len(message_split)-1]
+    player_name = message.replace(stat_time, '').strip()
+    player_data = requests.get(NBA_API)
+    player_data = player_data.json()['league']['standard']
+    player_data = list(filter(
+        lambda player: player['firstName'].lower() + ' ' + player['lastName'].lower() == player_name.lower(), player_data))[0]
+    player_id = player_data['personId']
+    display_name = player_data['firstName'] + ' ' + player_data['lastName']
+    response = requests.get(
+        BASE + f'player/{player_id}_basic_{stat_time}').json()
+    embed = discord.Embed(
+        title=display_name,
+        color=discord.Color.blue()
+    )
+    embed.set_image(
+        url=f"https://cdn.nba.com/headshots/nba/latest/1040x760/{player_id}.png")
+    embed.add_field(name="PTS",
+                    value=response['ppg'])
+    embed.add_field(name="AST",
+                    value=response['apg'])
+    embed.add_field(name="REB",
+                    value=response['rpg'])
+    embed.add_field(name="FG%",
+                    value=response['fgp'])
+    embed.add_field(name='FT%',
+                    value=response['ftp'])
+    embed.add_field(name="3P%",
+                    value=response['tpp'])
+
+    return embed
+
+# parse !gopher-advanced command and creates embed
+
+
+def parse_command_advanced(message):
+    message = message[17:]
+    message_split = message.split(' ')
+    stat_time = message_split[len(message_split)-1]
+    player_name = message.replace(stat_time, '').strip()
+    player_data = requests.get(NBA_API)
+    player_data = player_data.json()['league']['standard']
+    player_data = list(filter(
+        lambda player: player['firstName'].lower() + ' ' + player['lastName'].lower() == player_name.lower(), player_data))[0]
+    player_id = player_data['personId']
+    display_name = player_data['firstName'] + ' ' + player_data['lastName']
+    response = requests.get(
+        BASE + f'player/{player_id}_advanced_{stat_time}').json()
+    embed = discord.Embed(
+        title=display_name,
+        color=discord.Color.blue()
+    )
+    embed.set_image(
+        url=f"https://cdn.nba.com/headshots/nba/latest/1040x760/{player_id}.png")
+    embed.add_field(name="BPM",
+                    value=response['BPM'])
+    embed.add_field(name="OBPM",
+                    value=response['OBPM'])
+    embed.add_field(name="DBPM",
+                    value=response['DBPM'])
+    embed.add_field(name="WS",
+                    value=response['WS'])
+    embed.add_field(name='OWS',
+                    value=response['OWS'])
+    embed.add_field(name="DWS",
+                    value=response['DWS'])
+    return embed
+
+
+@ client.event
 async def on_ready():
     for guild in client.guilds:
         if guild.name == GUILD:
@@ -27,36 +100,17 @@ async def on_ready():
     )
 
 
-@client.event
+@ client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
-    # Player Stat_type Stat_time_type
-    message_split = message.content.split('/')
-    if message_split[0] == '!gopher':
-        player_data = requests.get(NBA_API)
-        player_data = player_data.json()['league']['standard']
-        player_name = message_split[1]
-        stat_type = message_split[2]
-        stat_time = message_split[3]
-        player_data = list(filter(
-            lambda player: player['firstName'].lower() + ' ' + player['lastName'].lower() == player_name.lower(), player_data))[0]
-        player_id = player_data['personId']
-        response = requests.get(
-            BASE + f'player/{player_id}_{stat_type}_{stat_time}').json()
-        embed = discord.Embed(
-            title=player_name,
-            color=discord.Color.blue()
-        )
-        if (stat_type == 'basic'):
-            embed.add_field(name="PTS",
-                            value=response['ppg'])
-            embed.add_field(name="AST",
-                            value=response['apg'])
-            embed.add_field(name="REB",
-                            value=response['rpg'])
-        await message.channel.send(embed=embed)
+    message_split = message.content.split(' ')
+    command = message_split[0]
+    if command == '!gopher-basic':
+        embed = parse_command_basic(message.content)
+    elif command == '!gopher-advanced':
+        embed = parse_command_advanced(message.content)
+    await message.channel.send(embed=embed)
 
 
 client.run(TOKEN)
